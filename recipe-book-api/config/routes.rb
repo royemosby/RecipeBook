8a499dfc2114cc40cfd56be439915fd0c3cea541
Roy Mosby <ego.madking@gmail.com>
2021-06-28 16:09:49 -0400

    draws login

--- recipe-book-api/config/routes.rb
+++ recipe-book-api/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+  #TODO: clean up routes
   resources :recipes
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/login', to: 'auth#create'
 end
