e5081a2020a7dd96c705cab480e2370aa3c4ef74
Roy Mosby <ego.madking@gmail.com>
2021-06-28 16:09:02 -0400

    adds authentication cycle

--- recipe-book-api/app/controllers/auth_controller.rb
+++ recipe-book-api/app/controllers/auth_controller.rb
@@ -1,2 +1,21 @@
 class AuthController < ApplicationController
+
+  skip_before_action :authenticate, only: [:create]
+
+  def create
+    #byebug
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      #encode_token resides in application controller
+      @token = encode_token({user_id: @user.id})
+      render json: {user: UserSerializer.new(@user), jwt: @token}
+    else
+      render json: {message: "Invalid username or password"}, status: :unauthorized
+    end
+  end
+
+  def login_params
+    params.require(:user).permit(:username, :password)
+  end
+  
 end
