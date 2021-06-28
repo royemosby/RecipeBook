098d3b6d6a6eb6bf9921027f6df209ead2cd8c6c
Roy Mosby <ego.madking@gmail.com>
2021-06-28 16:10:03 -0400

    udpates schema with recipes

--- recipe-book-api/db/schema.rb
+++ recipe-book-api/db/schema.rb
@@ -10,11 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_28_191634) do
+ActiveRecord::Schema.define(version: 2021_06_28_192324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "recipes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.text "ingredients"
+    t.text "instructions"
+    t.integer "servings"
+    t.uuid "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "username"
@@ -25,4 +37,5 @@ ActiveRecord::Schema.define(version: 2021_06_28_191634) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "recipes", "users"
 end
