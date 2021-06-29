62842fe8701f6193a1ed2a119e63fea091ee3942
Roy Mosby <ego.madking@gmail.com>
2021-06-29 10:00:07 -0400

    adds expected user actions

--- recipe-book-frontend/src/reducers/user.js
+++ recipe-book-frontend/src/reducers/user.js
@@ -1,12 +1,57 @@
 const initialState = {
   id: '',
   username: '',
+  password: '',
   email: '',
   token: '',
+  message: '',
+  isAuthModalOpen: false,
+  isLoggedIn: false,
+  requesting: false,
 }
 
 function user(state = initialState, action) {
-  return state
+  switch (action.type) {
+    case 'ADD_CREDENTIALS':
+      return {
+        ...state,
+        username: action.credentials.username,
+        password: action.credentials.password,
+        email: action.credentials.email,
+      }
+    case 'TOGGLE_LOGIN_MODAL':
+      return {
+        ...state,
+        isAuthModalOpen: !state.isAuthModalOpen,
+      }
+    case 'SEND_CREATE_USER':
+      return {
+        ...state,
+        requesting: true,
+        password: '',
+      }
+    case 'NEW_USER_ERROR':
+      console.dir(action)
+      return {
+        ...state,
+        requesting: false,
+      }
+    case 'ADD_USER':
+      console.dir(action)
+      return {
+        ...state,
+        requesting: false,
+        token: action.response.jwt,
+        id: action.response.user.id,
+        isLoggedIn: true,
+      }
+    case 'LOG_OUT':
+      return {
+        ...initialState,
+      }
+    default:
+      return state
+  }
 }
 
 export { user }
