6d203480e259a3522c5907de8f26798b8675137f
Roy Mosby <ego.madking@gmail.com>
2021-06-29 18:54:58 -0400

    connects to store

--- recipe-book-frontend/src/recipes/EditRecipe.js
+++ recipe-book-frontend/src/recipes/EditRecipe.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import RecipeForm from './components/RecipeForm'
 
 class EditRecipe extends Component {
   render() {
-    return <h1>EditRecipe</h1>
+    return <h1>Edit {this.props.recipe.name}</h1>
   }
 }
 
-export default EditRecipe
+const mapStateToProps = (state, ownProps) => {
+  return {
+    recipe: state.recipes.find((r) => r.id === ownProps.match.params.recipeId),
+  }
+}
+
+export default connect(mapStateToProps)(EditRecipe)
