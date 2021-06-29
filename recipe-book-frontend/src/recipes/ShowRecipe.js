aa5da54338adf079abf2ef0f536ebf527265cfc8
Roy Mosby <ego.madking@gmail.com>
2021-06-28 21:31:29 -0400

    adds div wrapper

--- recipe-book-frontend/src/recipes/ShowRecipe.js
+++ recipe-book-frontend/src/recipes/ShowRecipe.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react"
+import NoMatch from "../common/NoMatch"
 
 class ShowRecipe extends Component{
   render(){
     return(
+      <div>
       <h1>ShowRecipe</h1>
+        {/* {<NoMatch/>} */}
+      </div>
     )
   }
 }
