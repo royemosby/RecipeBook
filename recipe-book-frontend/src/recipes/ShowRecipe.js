5ae01d706eb070e81c03cb9a7c3e969bc32e4800
Roy Mosby <ego.madking@gmail.com>
2021-06-29 17:50:12 -0400

    maps state to props

--- recipe-book-frontend/src/recipes/ShowRecipe.js
+++ recipe-book-frontend/src/recipes/ShowRecipe.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
-import NoMatch from '../common/NoMatch'
+import { connect } from 'react-redux'
 
 class ShowRecipe extends Component {
   render() {
     return (
       <div>
         <h1>ShowRecipe</h1>
-        {<NoMatch />}
       </div>
     )
   }
 }
 
-export default ShowRecipe
+const mapStateToProps = (state) => {
+  return {
+    recipes: state.recipes,
+  }
+}
+
+export default connect(mapStateToProps)(ShowRecipe)
