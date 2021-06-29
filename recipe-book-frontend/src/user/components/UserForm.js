a8835a5d84c466eab97b47c72ae28afe7f429430
Roy Mosby <ego.madking@gmail.com>
2021-06-28 23:27:29 -0400

    adds onChange to each input

--- recipe-book-frontend/src/user/components/UserForm.js
+++ recipe-book-frontend/src/user/components/UserForm.js
@@ -15,7 +15,9 @@ class UserForm extends Component {
 
   handleSubmit = (evt) => {
     evt.preventDefault()
+    this.props.submit(this.state)
   }
+
   render() {
     return (
       <div className="prez">
@@ -23,23 +25,34 @@ class UserForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <div>
             <label htmlFor="username">Username: </label>
-            <input type="text" name="username" value={this.state.password} />
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
             <label htmlFor="email">Email: </label>
-            <input type="text" name="password" value={this.props.email} />
+            <input
+              type="text"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
             <label htmlFor="password">Password: </label>
             <input
               type="password"
               name="password"
-              value={this.state.password}
+              value={this.password}
+              onChange={this.handleChange}
             />
           </div>
           <div>
             <input type="submit" value="Register with RecipeBook" />
-            <button type="button" onClick={this.handleCancel}>
+            <button type="button" onClick={this.props.handleCancel}>
               Cancel
             </button>
           </div>
