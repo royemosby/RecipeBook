4e636ee96c73ffdd9ee0431f8760ba7c797ed7cf
Roy Mosby <ego.madking@gmail.com>
2021-06-29 16:51:50 -0400

    adds validation for needed fields

--- recipe-book-api/app/models/recipe.rb
+++ recipe-book-api/app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :serves, numericality: {greater_than: 0}
+  
 end
