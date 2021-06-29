b5a9594a31874ba2f0e32498593dd65fd663cabe
Roy Mosby <ego.madking@gmail.com>
2021-06-29 11:41:21 -0400

    adds form and authentication process

--- recipe-book-frontend/src/main/AuthModal.js
+++ recipe-book-frontend/src/main/AuthModal.js
@@ -6,7 +6,6 @@ class AuthModal extends Component {
   state = {
     username: '',
     password: '',
-    email: '',
   }
 
   handleChange = (evt) => {
@@ -15,18 +14,43 @@ class AuthModal extends Component {
     })
   }
 
-  submit = (credentials) => {
+  handleSubmit = (evt) => {
+    evt.preventDefault()
     console.log('submitted')
-    console.dir(credentials)
-    //this.props.addCredentials(this.state)
-    //TODO: Change this this.props.submit(this.state)
+    this.props.addCredentials(this.state)
+    this.props.authenticate()
   }
 
   render() {
     return (
       <div>
-        <h1>AuthModal</h1>
-        <form></form>
+        <h1>Login to RecipeBook</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="username">Username: </label>
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              name="password"
+              value={this.password}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <input type="submit" value="Login to RecipeBook" />
+            <button type="button" onClick={this.props.handleCancel}>
+              Cancel
+            </button>
+          </div>
+        </form>
       </div>
     )
   }
@@ -38,6 +62,7 @@ const mapDispatchToProps = (dispatch) => {
       dispatch({ type: 'ADD_CREDENTIALS', credentials })
     },
     authenticate: () => dispatch(authenticate()),
+    handleCancel: () => dispatch({ type: 'CANCEL_AUTH' }),
   }
 }
 
