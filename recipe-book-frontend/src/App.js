3dfdfac4d98ebf57d0d55fe482f88ad9661390fc
Roy Mosby <ego.madking@gmail.com>
2021-06-28 21:29:54 -0400

    Adds routes

--- recipe-book-frontend/src/App.js
+++ recipe-book-frontend/src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Header from './common/Header'
+import MainContainer from './main/MainContainer'
+import ShowUser from './user/ShowUser'
+import EditUser from './user/EditUser'
+import NewUser from './user/NewUser'
+import IndexRecipes from './recipes/IndexRecipes'
+import ShowRecipe from './recipes/ShowRecipe'
+import NewRecipe from './recipes/NewRecipe'
+import EditRecipe from './recipes/EditRecipe'
+import NoMatch from './common/NoMatch'
 
 function App() {
   return (
-    <div className="App">
-      This my app YO.
+    <div id="appWrapper">
+      <Router>
+        <div>
+          <Header/>
+          <Switch>
+            <Route exact path="/" component={MainContainer}/>
+            <Route exact path="/user" component={ShowUser}/>
+            <Route exact path="/user/edit" component={EditUser}/>
+            <Route exact path="/user/new" component={NewUser}/>
+            <Route exact path="/recipes" component={IndexRecipes}/>
+            <Route exact path="/recipes/new" component={NewRecipe}/>
+            <Route path="/recipes/:recipeId" component={ShowRecipe}/>
+            <Route exact path="/recipes/:recipeId/edit" component={EditRecipe}/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
