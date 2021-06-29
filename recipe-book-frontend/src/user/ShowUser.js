d5a9740c19d5d703275beefa745fe9eea490682e
Roy Mosby <ego.madking@gmail.com>
2021-06-29 15:21:00 -0400

    adds props and content to show user

--- recipe-book-frontend/src/user/ShowUser.js
+++ recipe-book-frontend/src/user/ShowUser.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 class ShowUser extends Component {
   render() {
-    return <h1>ShowUser</h1>
+    return (
+      <div className="skeleton">
+        <h1>Username: {this.props.user.username}</h1>
+        <h2>Email: {this.props.user.email || 'None found'}</h2>
+        <h2>{`${this.props.recipes.length} recipes`}</h2>
+      </div>
+    )
   }
 }
 
-export default ShowUser
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+    recipes: state.recipes,
+  }
+}
+
+export default connect(mapStateToProps)(ShowUser)
