d8c1ed85d7e3b69ca5d3909da47ae624e946fbad
Roy Mosby <ego.madking@gmail.com>
2021-06-29 13:11:40 -0400

    populates recipe card with props

--- recipe-book-frontend/src/recipes/components/RecipeCard.js
+++ recipe-book-frontend/src/recipes/components/RecipeCard.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const RecipeCard = (props) => {
-  return <h1>RecipeCard</h1>
+const RecipeCard = ({ recipe }) => {
+  return (
+    <Link to={`/recipes/${recipe.id}`}>
+      <div className="prez">
+        <h3>{recipe.name}</h3>
+        <p>{recipe.description}</p>
+      </div>
+    </Link>
+  )
 }
 
 export default RecipeCard
