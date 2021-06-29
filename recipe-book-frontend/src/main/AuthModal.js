0cd1afef7a9119d0c48bdf43d8df1d3c751764d7
Roy Mosby <ego.madking@gmail.com>
2021-06-29 09:58:39 -0400

    starts auth modal

--- recipe-book-frontend/src/main/AuthModal.js
+++ recipe-book-frontend/src/main/AuthModal.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { authenticate } from '../adapters/user'
 
-const AuthModal = (props) => {
-  return <h1>AuthModal</h1>
+class AuthModal extends Component {
+  state = {
+    username: '',
+    password: '',
+    email: '',
+  }
+
+  handleChange = (evt) => {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    })
+  }
+
+  submit = (credentials) => {
+    console.log('submitted')
+    console.dir(credentials)
+    //this.props.addCredentials(this.state)
+    //TODO: Change this this.props.submit(this.state)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>AuthModal</h1>
+        <form></form>
+      </div>
+    )
+  }
 }
 
-export default AuthModal
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addCredentials: (credentials) => {
+      dispatch({ type: 'ADD_CREDENTIALS', credentials })
+    },
+    authenticate: () => dispatch(authenticate()),
+  }
+}
+
+export default connect(null, mapDispatchToProps)(AuthModal)
