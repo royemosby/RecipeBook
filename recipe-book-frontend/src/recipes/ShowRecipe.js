a6f42f1942261fc0c1c6a7476806bc882881dd78
Roy Mosby <ego.madking@gmail.com>
2021-06-29 18:55:15 -0400

    connects to store and fills in props

--- recipe-book-frontend/src/recipes/ShowRecipe.js
+++ recipe-book-frontend/src/recipes/ShowRecipe.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 class ShowRecipe extends Component {
+  handleDelete = () => {
+    alert('fake d3l3t0rz')
+  }
   render() {
     return (
-      <div>
+      <div className="skeleton">
         <h1>ShowRecipe</h1>
+        <h2>{this.props.recipe.name}</h2>
+        <div className="left-aligned">
+          <h3>Description</h3>
+          <p>{this.props.recipe.description}</p>
+          <h3>Ingredients</h3>
+          <p className="newline-wrap">{this.props.recipe.ingredients}</p>
+          <h3>Instructions</h3>
+          <p className="newline-wrap">{this.props.recipe.instructions}</p>
+        </div>
+        <div>
+          <Link to={`/recipes/${this.props.recipe.id}/edit`} className="linky">
+            Edit Recipe
+          </Link>
+          <Link
+            to="#"
+            className={['linky', 'delete'].join(' ')}
+            onClick={this.handleDelete}>
+            Delete Recipe
+          </Link>
+        </div>
       </div>
     )
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
   return {
-    recipes: state.recipes,
+    recipe: state.recipes.find((r) => r.id === ownProps.match.params.recipeId),
   }
 }
 
