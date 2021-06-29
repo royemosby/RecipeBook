8a790c04d6cb6c6b6d5d9946fe84b74d4b31c1b6
Roy Mosby <ego.madking@gmail.com>
2021-06-29 16:54:09 -0400

    adds redirect when logged in

--- recipe-book-frontend/src/user/NewUser.js
+++ recipe-book-frontend/src/user/NewUser.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react'
 import UserForm from './components/UserForm'
 import { connect } from 'react-redux'
 import { createUser } from '../adapters/user'
+import { withRouter } from 'react-router-dom'
 
 class NewUser extends Component {
   submit = (credentials) => {
     this.props.addCredentials(credentials)
     this.props.createUser()
   }
+
+  redirectWhenLoggedIn = (props) => {
+    if (props.isLoggedIn) {
+      props.history.push('/')
+    }
+  }
+
   render() {
     return (
       <div className="skeleton">
         <h1>NewUser</h1>
         <UserForm submit={this.submit} cancel={this.props.cancel} />
+        {this.redirectWhenLoggedIn(this.props)}
       </div>
     )
   }
@@ -27,4 +36,10 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default connect(null, mapDispatchToProps)(NewUser)
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(NewUser))
