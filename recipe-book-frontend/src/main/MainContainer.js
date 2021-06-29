446f0f240f90e88331b7855b341c4d0265b48492
Roy Mosby <ego.madking@gmail.com>
2021-06-28 21:31:17 -0400

    adds div wrapper

--- recipe-book-frontend/src/main/MainContainer.js
+++ recipe-book-frontend/src/main/MainContainer.js
@@ -3,7 +3,9 @@ import React, { Component } from "react"
 class MainContainer extends Component{
   render(){
     return(
-      <h1>MainContainer</h1>
+      <div className="skeleton">
+        <h1>MainContainer</h1>
+      </div>
     )
   }
 }
