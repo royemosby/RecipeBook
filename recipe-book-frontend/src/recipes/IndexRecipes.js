0e545e561afe9a4cfcaf7db98ce355bbb9eeefb6
Roy Mosby <ego.madking@gmail.com>
2021-06-29 17:07:06 -0400

    removes historyPush because of a maximum call stack error when logging in

--- recipe-book-frontend/src/recipes/IndexRecipes.js
+++ recipe-book-frontend/src/recipes/IndexRecipes.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import RecipeCard from './components/RecipeCard'
-import { withRouter } from 'react-router-dom'
 
 class IndexRecipes extends Component {
   renderRecipeCards = () => {
@@ -13,16 +12,10 @@ class IndexRecipes extends Component {
   render() {
     return (
       <div className="skeleton">
-        {this.redirectWhenLoggedOut(this.props)}
         <h1>IndexRecipes</h1>
         {this.renderRecipeCards()}
       </div>
     )
-  }
-  redirectWhenLoggedOut = (props) => {
-    if (!props.isLoggedIn) {
-      props.history.push('/')
-    }
   }
 }
 
@@ -32,4 +25,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(withRouter(IndexRecipes))
+export default connect(mapStateToProps)(IndexRecipes)
