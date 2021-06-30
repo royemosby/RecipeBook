ba41317a61016ee8d9f1511cbc9ce654e3273f20
Roy Mosby <ego.madking@gmail.com>
2021-06-29 22:24:42 -0400

    adds actions

--- recipe-book-frontend/src/reducers/recipes.js
+++ recipe-book-frontend/src/reducers/recipes.js
@@ -4,6 +4,16 @@ function recipes(state = [], action) {
       return [...state, ...action.recipes]
     case 'LOG_OUT':
       return []
+    case 'SEND_CREATE_RECIPE':
+      return [...state]
+    case 'SEND_UPDATE_RECIPE':
+      return [...state]
+    case 'CREATE_RECIPE_ERROR':
+    case 'UPDATE_RECIPE_ERROR':
+      return [...state]
+    case 'CREATE_RECIPE':
+      return [...state]
+    case 'UPDATE_RECIPE':
     default:
       return state
   }
