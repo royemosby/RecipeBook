a293754ae92e5c702e458570219c1f74381d3a4e
Roy Mosby <ego.madking@gmail.com>
2021-06-30 08:26:03 -0400

    implements new recipe submission

--- recipe-book-frontend/src/recipes/NewRecipe.js
+++ recipe-book-frontend/src/recipes/NewRecipe.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 import RecipeForm from './components/RecipeForm'
+import { connect } from 'react-redux'
+import { createRecipe } from '../adapters/recipes'
 
 class NewRecipe extends Component {
+  handleSubmit = (state) => {
+    this.props.updateLocalRecipe(state)
+    this.props.createRecipe()
+  }
+
   render() {
     return (
-      <div>
-        <h1>NewRecipe</h1>
-        <RecipeForm />
+      <div className="skeleton">
+        <h1>New Recipe</h1>
+        <RecipeForm handleSubmit={this.handleSubmit} />
       </div>
     )
   }
 }
 
-export default NewRecipe
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateLocalRecipe: (recipe) => {
+      dispatch({ type: 'CREATE_HOLDER_RECIPE', recipe })
+    },
+    createRecipe: () => dispatch(createRecipe()),
+  }
+}
+
+export default connect(null, mapDispatchToProps)(NewRecipe)
