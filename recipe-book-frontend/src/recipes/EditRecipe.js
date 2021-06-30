30fe58e091a2482ee1a10674ebe9814174a6e20b
Roy Mosby <ego.madking@gmail.com>
2021-06-29 21:15:51 -0400

    adds form to edit recipe component

--- recipe-book-frontend/src/recipes/EditRecipe.js
+++ recipe-book-frontend/src/recipes/EditRecipe.js
@@ -3,8 +3,16 @@ import { connect } from 'react-redux'
 import RecipeForm from './components/RecipeForm'
 
 class EditRecipe extends Component {
+  handleSubmit = (state) => {
+    //do the thing with dispatch
+  }
   render() {
-    return <h1>Edit {this.props.recipe.name}</h1>
+    return (
+      <div className="skeleton">
+        <h1>Edit {this.props.recipe.name}</h1>
+        <RecipeForm recipe={this.props.recipe} />
+      </div>
+    )
   }
 }
 
@@ -12,6 +20,10 @@ const mapStateToProps = (state, ownProps) => {
   return {
     recipe: state.recipes.find((r) => r.id === ownProps.match.params.recipeId),
   }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {}
 }
 
 export default connect(mapStateToProps)(EditRecipe)
