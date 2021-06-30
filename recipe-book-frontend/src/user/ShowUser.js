1a8a08f2c250c3df17f29a04575ffb8b3897e096
Roy Mosby <ego.madking@gmail.com>
2021-06-29 21:17:57 -0400

    removes redirect when logged out: interferes with rendering

--- recipe-book-frontend/src/user/ShowUser.js
+++ recipe-book-frontend/src/user/ShowUser.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link, withRouter } from 'react-router-dom'
 
 class ShowUser extends Component {
   render() {
     return (
       <div className="skeleton">
-        {this.redirectWhenLoggedOut(this.props)}
         <h1>Username: {this.props.user.username}</h1>
         <h2>Email: {this.props.user.email || 'None found'}</h2>
         <h2>{`${this.props.recipes.length} recipes`}</h2>
       </div>
     )
-  }
-
-  redirectWhenLoggedOut = (props) => {
-    if (!props.isLoggedIn) {
-      props.history.push('/')
-    }
   }
 }
 
@@ -28,4 +20,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(withRouter(ShowUser))
+export default connect(mapStateToProps)(ShowUser)
