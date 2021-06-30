054f644439247bdfc659a730a234e9accbebee73
Roy Mosby <ego.madking@gmail.com>
2021-06-30 07:10:45 -0400

    adds functionality to UPDATE_RECIPE, moves ui actions to ui reducer

--- recipe-book-frontend/src/reducers/recipes.js
+++ recipe-book-frontend/src/reducers/recipes.js
@@ -4,16 +4,22 @@ function recipes(state = [], action) {
       return [...state, ...action.recipes]
     case 'LOG_OUT':
       return []
-    case 'SEND_CREATE_RECIPE':
-      return [...state]
-    case 'SEND_UPDATE_RECIPE':
-      return [...state]
-    case 'CREATE_RECIPE_ERROR':
-    case 'UPDATE_RECIPE_ERROR':
-      return [...state]
     case 'CREATE_RECIPE':
-      return [...state]
+      return [...state, action.response]
     case 'UPDATE_RECIPE':
+      console.log('UPDATE_RECIPE')
+      console.dir(action.response)
+      console.dir(state)
+      const target = state.findIndex((r) => {
+        return r.id === action.response.id
+      })
+      if (target >= 0) {
+        const prefix = state.slice(0, target)
+        const suffix = state.slice(target + 1)
+        return [...prefix, action.response, ...suffix]
+      } else {
+        return state
+      }
     default:
       return state
   }
