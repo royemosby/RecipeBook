656b57009c523522f09d49452d1f61abe42a473a
Roy Mosby <ego.madking@gmail.com>
2021-06-30 09:14:30 -0400

    convert AuthModal to stateless component

--- recipe-book-frontend/src/main/AuthModal.js
+++ recipe-book-frontend/src/main/AuthModal.js
@@ -1,69 +1,37 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { authenticate } from '../adapters/user'
+import React from 'react'
 
-class AuthModal extends Component {
-  state = {
-    username: '',
-    password: '',
-  }
-
-  handleChange = (evt) => {
-    this.setState({
-      [evt.target.name]: evt.target.value,
-    })
-  }
-
-  handleSubmit = (evt) => {
-    evt.preventDefault()
-    console.log('submitted')
-    this.props.addCredentials(this.state)
-    this.props.authenticate()
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Login to RecipeBook</h1>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label htmlFor="username">Username: </label>
-            <input
-              type="text"
-              name="username"
-              value={this.state.username}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password: </label>
-            <input
-              type="password"
-              name="password"
-              value={this.password}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <input type="submit" value="Login to RecipeBook" />
-            <button type="button" onClick={this.props.handleCancel}>
-              Cancel
-            </button>
-          </div>
-        </form>
-      </div>
-    )
-  }
+const AuthModal = (props) => {
+  return (
+    <div class="prez">
+      <h1>Login to RecipeBook</h1>
+      <form onSubmit={props.handleSubmit}>
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            name="username"
+            value={props.userInfo.username}
+            onChange={props.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            name="password"
+            value={props.userInfo.password}
+            onChange={props.handleChange}
+          />
+        </div>
+        <div>
+          <input type="submit" value="Login to RecipeBook" />
+          <button type="button" onClick={props.handleCancel}>
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    addCredentials: (credentials) => {
-      dispatch({ type: 'ADD_CREDENTIALS', credentials })
-    },
-    authenticate: () => dispatch(authenticate()),
-    handleCancel: () => dispatch({ type: 'CANCEL_AUTH' }),
-  }
-}
-
-export default connect(null, mapDispatchToProps)(AuthModal)
+export default AuthModal
--- recipe-book-frontend/src/main/MainContainer.js
+++ recipe-book-frontend/src/main/MainContainer.js
@@ -2,13 +2,41 @@ import React, { Component } from 'react'
 import AuthModal from './AuthModal'
 import IndexRecipes from '../recipes/IndexRecipes'
 import { connect } from 'react-redux'
+import { authenticate } from '../adapters/user'
 
 class MainContainer extends Component {
+  state = {
+    username: '',
+    password: '',
+  }
+
+  handleChange = (evt) => {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    })
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+    console.log('submitted')
+    this.props.addCredentials(this.state)
+    this.props.authenticate()
+  }
+
   render() {
     return (
       <div className="skeleton">
         <h1 className="title ">RecipeBook</h1>
-        {this.props.isAuthModalOpen ? <AuthModal /> : <></>}
+        {this.props.isAuthModalOpen ? (
+          <AuthModal
+            handleSubmit={this.handleSubmit}
+            userInfo={this.state}
+            handleChange={this.handleChange}
+            handleCancel={this.props.handleCancel}
+          />
+        ) : (
+          <></>
+        )}
         {this.props.isLoggedIn ? (
           <IndexRecipes />
         ) : (
@@ -24,6 +52,16 @@ class MainContainer extends Component {
   }
 }
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addCredentials: (credentials) => {
+      dispatch({ type: 'ADD_CREDENTIALS', credentials })
+    },
+    authenticate: () => dispatch(authenticate()),
+    handleCancel: () => dispatch({ type: 'CANCEL_AUTH' }),
+  }
+}
+
 const mapStateToProps = (state) => {
   return {
     isAuthModalOpen: state.ui.isAuthModalOpen,
@@ -31,4 +69,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(MainContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(MainContainer)
