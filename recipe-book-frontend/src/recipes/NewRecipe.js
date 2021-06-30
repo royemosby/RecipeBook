e8316ec6a99bb1489a337368782db33585441693
Roy Mosby <ego.madking@gmail.com>
2021-06-29 21:16:08 -0400

    adds form to new recipe

--- recipe-book-frontend/src/recipes/NewRecipe.js
+++ recipe-book-frontend/src/recipes/NewRecipe.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import RecipeForm from './components/RecipeForm'
 
 class NewRecipe extends Component {
   render() {
-    return <h1>NewRecipe</h1>
+    return (
+      <div>
+        <h1>NewRecipe</h1>
+        <RecipeForm />
+      </div>
+    )
   }
 }
 
